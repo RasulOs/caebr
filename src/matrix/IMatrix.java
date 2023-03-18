65f117b495f5d97acac67390efd6b37fa96a81fc
rasul.osmanbeyli@gmail.com <rasul.osmanbeyli@gmail.com>
2023-03-18 19:14:05 +0300

    API changes.

--- src/matrix/IMatrix.java
+++ src/matrix/IMatrix.java
@@ -1,5 +1,7 @@
 package matrix;
 
+import java.util.List;
+
 public interface IMatrix<T extends Number> {
 
     IMatrix<T> add(T[][] matrix);
@@ -15,4 +17,49 @@ public interface IMatrix<T extends Number> {
     IMatrix<T> transpose();
 
     IMatrix<T> dot(IMatrix<T> iMatrix);
+
+    Double sum();
+
+    Double mean();
+
+    Double max();
+
+    Double min();
+
+    Double median();
+
+    List<Double> mode();
+
+    Double variance();
+
+    Double standardDeviation();
+
+    Double range();
+
+    IMatrix<T> sort();
+
+    IMatrix<T> minMaxNormalization(long min, long max);
+
+    IMatrix<T> minMaxNormalization();
+
+    IMatrix<T> zScoreStandardization();
+
+    int getRowNumber();
+
+    int getColumnNumber();
+
+    boolean isSquare();
+
+    boolean isSymmetric();
+
+    boolean isIdentity();
+
+    boolean isDiagonal();
+
+    boolean isAntiDiagonal();
+
+    Double determinant();
+
+    IMatrix<T> inverse();
+
 }
